e64e9d74ba0fc4441e8ee99b2bc3ed60c7061a9a
Mercado Felix Luis Felipe <13170308@itculiacan.edu.mx>
2019-04-30 13:58:57 -0600

    Listado de clientes y controlador

--- crud_node/src/rutas/index.js
+++ crud_node/src/rutas/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const rutas = express.Router();
 
-rutas.get("/", (req,res) => {
-    res.send("Hola Mundo nwn, pero en Node owo (Literal xd) pd. JAJAJAJA XD")
-});
+const customerController = require('../controllers/customerController');
+
+// rutas.get("/", (req,res) => {
+//     res.send("Hola Mundo en Node")
+// });
+
+rutas.get('/', customerController.list);
+// rutas.post('/add', customerController.save);
+// rutas.get('/delete', customerController.delete);
+// rutas.get('/update/:id', customerController.edit);
+// rutas.post('/update/:id', customerController.update);
 
 module.exports = rutas;
